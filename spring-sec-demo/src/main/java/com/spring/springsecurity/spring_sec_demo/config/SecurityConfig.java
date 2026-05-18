af335502f5f18bbda326fa93023d49994dfe60b6
Khush786Mohammad <khush786moh@gmail.com>
2026-05-18 08:21:33 +0530

    Adding User Security Config

--- spring-sec-demo/src/main/java/com/spring/springsecurity/spring_sec_demo/config/SecurityConfig.java
+++ spring-sec-demo/src/main/java/com/spring/springsecurity/spring_sec_demo/config/SecurityConfig.java
@@ -5,30 +5,51 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		
-		//without lambda way.
-		Customizer<CsrfConfigurer<HttpSecurity>> customizer = new Customizer<CsrfConfigurer<HttpSecurity>>(){
-			@Override
-			public void customize(CsrfConfigurer<HttpSecurity> configurer) {
-				configurer.disable();
-			}
-		};
-		
-		//with lambda way
+
+		// without lambda way.
+//		Customizer<CsrfConfigurer<HttpSecurity>> customizer = new Customizer<CsrfConfigurer<HttpSecurity>>(){
+//			@Override
+//			public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+//				configurer.disable();
+//			}
+//		};
+
+		// with lambda way
 		http.csrf(csrf -> csrf.disable());
 		http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
 		http.httpBasic(Customizer.withDefaults());
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user1 = User
+							.withDefaultPasswordEncoder()
+							.username("khush")
+							.password("1234")
+							.roles("USER")
+							.build();
+		UserDetails admin = User
+							.withDefaultPasswordEncoder()
+							.username("admin")
+							.password("admin@123")
+							.roles("ADMIN")
+							.authorities("SUPER_ADMIN")
+							.build();
+		return new InMemoryUserDetailsManager(user1, admin);
 	}
 }
