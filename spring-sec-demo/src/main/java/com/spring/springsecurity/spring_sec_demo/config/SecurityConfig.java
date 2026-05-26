d4415f69ccdeefcfe50fa5256c9fdeea49bf39a0
Khush786Mohammad <khush786moh@gmail.com>
2026-05-26 19:05:57 +0530

    Remove unused imports

--- spring-sec-demo/src/main/java/com/spring/springsecurity/spring_sec_demo/config/SecurityConfig.java
+++ spring-sec-demo/src/main/java/com/spring/springsecurity/spring_sec_demo/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.spring.springsecurity.spring_sec_demo.config;
 
-import com.spring.springsecurity.spring_sec_demo.filters.JwtFilter;
-
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +15,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.spring.springsecurity.spring_sec_demo.filters.JwtFilter;
 
 @Configuration
 @EnableWebSecurity
